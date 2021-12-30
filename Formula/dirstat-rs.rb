2fd20adf2d658fa9b431ba73ae422aaca2d8d369
William Scullion <williamscullion@gmail.com>
2021-12-30 16:52:09 -0500

    fix name

--- Formula/dirstat-rs.rb
+++ Formula/dirstat-rs.rb
@@ -1,4 +1,4 @@
-class Dirstat < Formula
+class DirstatRs < Formula
     desc "Fast, cross-platform disk usage CLI"
     homepage "https://github.com/scullionw/dirstat-rs"
     url "https://github.com/scullionw/dirstat-rs/releases/download/v0.3.5/dirstat-rs-v0.3.5-universal-apple-darwin.tar.gz"

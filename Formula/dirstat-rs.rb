1671bd7ce55542e1cf336f611dcd22218b81b144
William Scullion <williamscullion@gmail.com>
2021-12-30 20:17:49 -0500

    dirstat-rs 0.3.6

--- Formula/dirstat-rs.rb
+++ Formula/dirstat-rs.rb
@@ -1,9 +1,9 @@
 class DirstatRs < Formula
     desc "Fast, cross-platform disk usage CLI"
     homepage "https://github.com/scullionw/dirstat-rs"
-    url "https://github.com/scullionw/dirstat-rs/releases/download/v0.3.5/dirstat-rs-v0.3.5-universal-apple-darwin.tar.gz"
-    sha256 "ca22511b23b2128c200c4ff112353e00066873e5f452dd5235099008a386c93c"
-    version "0.3.5"
+    url "https://github.com/scullionw/dirstat-rs/releases/download/v0.3.6/dirstat-rs-v0.3.6-universal-apple-darwin.tar.gz"
+    sha256 "faf9af3cb506f83e94da17ce1b1615bf32f14fac8446a78495ad049a5fd9c224"
+    version "0.3.6"
   
     def install
       bin.install "ds"
